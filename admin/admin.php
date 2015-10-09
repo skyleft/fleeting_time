26280ac739f7decde371239a73ccb84564d968f3
skyleft <skyleft123@gmail.com>
2015-10-10 07:22:15 +0800

    Initial commit

---
+++ admin/admin.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andy
+ * Date: 15/10/10
+ * Time: 上午7:17
+ */
---
+++ fleeting-time.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: Fleeting Time
+Plugin URI: http://andy-cheung.me
+Description: Add some fleeting time flag to your post according to the post time
+Version: 1.0
+Author: andy
+Author URI: http://andy-cheung.me
+License: GPL2
+*/
