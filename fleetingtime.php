501555d63ce0c100ca4ebb95ba964f247b186968
skyleft <skyleft123@gmail.com>
2015-10-10 21:01:22 +0800

    add i18n support

--- fleetingtime.php
+++ fleetingtime.php
@@ -14,18 +14,18 @@ License: GPL2
 
     require_once('admin/admin.php');
 
-    add_action('init','fleeting_init');
+    add_action('plugins_loaded','fleeting_init');
 
     if(is_admin()){
         add_action('admin_menu', 'fleeting_admin_menu');
     }
 
     function fleeting_init(){
-        load_plugin_textdomain( 'fleeting_time', basename( dirname( __FILE__ ) ) . '/languages' );
+        load_plugin_textdomain( 'fleetingtime', false,dirname(plugin_basename(__FILE__)).'/languages/' );
     }
 
     function fleeting_admin_menu(){
-        add_options_page(__('fleeting time'),__('fleeting time'),'administrator','fleeting-time','display_admin_page');
+        add_options_page(__('fleeting time'),__('fleeting time'),'administrator','fleetingtime','display_admin_page');
     }
 
 ?>
